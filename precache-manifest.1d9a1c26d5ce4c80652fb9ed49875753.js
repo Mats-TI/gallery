4d5e0872bad9601e006a0c593435e5ef7a951d33
unknown <thapelomatsaung101@gmail.com>
2020-10-21 11:53:02 +0200

    Updates

---
+++ precache-manifest.1d9a1c26d5ce4c80652fb9ed49875753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26767d6e2fee724e1a5d5a1c11670a57",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "e0fbca70fff6e27ca2d2",
+    "url": "/gallery/static/css/main.6a373417.chunk.css"
+  },
+  {
+    "revision": "1441b3c90829c2409095",
+    "url": "/gallery/static/js/2.b384828b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery/static/js/2.b384828b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0fbca70fff6e27ca2d2",
+    "url": "/gallery/static/js/main.717d249a.chunk.js"
+  },
+  {
+    "revision": "1160e6796d671babbaaa",
+    "url": "/gallery/static/js/runtime-main.7b22ee28.js"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/gallery/static/media/Comfortaa-VariableFont_wght.f40889ca.ttf"
+  }
+]);
