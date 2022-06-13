edfe962ed71d10033b3f6d2f079beecfcd392701
Mats-TI <thapelomatsaung101@gmail.com>
2022-06-14 00:41:16 +0200

    Modified the error handling mechanism

--- src/components/ErrorHandler.js
+++ src/components/ErrorHandler.js
@@ -13,11 +13,15 @@ class ErrorHandler extends React.Component{
         });
     
     }
+    reloadMethod=()=>{
+        window.location.reload()
+    }
     render(){
         if (this.state.hasError){
         return(
-            <div>
-                <h1>Something ran amok</h1>
+            <div className='errorPage'>
+                <h1>That did not go according to plan :(. Confirm the spelling of your query or search for another item</h1>
+                <button onClick={this.reloadMethod}>Refresh</button>
             </div>
         )
         }
